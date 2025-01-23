18ce4b655e34b08e6042958ff7c578bf8c2beac7
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-23 01:24:33 +0100

    *edit* front / add allowance page : adding spender name filed + retrieving services through context + unlimited amount of token button

--- resources/js/Pages/AddAllowance.tsx
+++ resources/js/Pages/AddAllowance.tsx
@@ -1,38 +1,49 @@
 import TokenPanel from "@/Components/TokenPanel/TokenPanel";
+import { useServices } from "@/hooks/useServices";
 import DashboardLayout from "@/Layouts/DashboardLayout";
 import AllowanceService from "@/services/AllowanceService";
 import ERC20TokenService from "@/services/ERC20TokenService";
-import HardhatService from "@/services/HardhatService";
+import MetaMaskService from "@/services/MetaMaskService";
+import { THexAddress } from "@/types/THexAddress";
 import { FormEvent, useEffect, useState } from "react";
 
 export default function AddAllowance() {
 
     const [supply, setSupply] = useState<string>()
+    const [unlimitedAmount, setUnlimitedAmount] = useState<boolean>(false)
+    const [walletAddress, setWalletAddress] = useState<THexAddress>()
+
+    const {metamaskService, erc20TokenService} = useServices()
 
     async function read(){
         try{
-            const hhs = new HardhatService()
-            const readSupply = await hhs.readERC20Supply("0x5FbDB2315678afecb367f032d93F642f64180aa3")
-            setSupply(readSupply)
             const as = new AllowanceService()
+
+            const metamaskAddress = await metamaskService.getWalletAddress()
+            if(metamaskAddress) {
+                setWalletAddress(metamaskAddress)
+                console.log("metamask address : ", metamaskAddress)
+            }
+
+            const readSupply = await erc20TokenService.getTotalSupply("0x5FbDB2315678afecb367f032d93F642f64180aa3")
+            if(readSupply) setSupply(readSupply)
             
             const receipt = await as.setAllowance({
                 contractAddress : "0x5FbDB2315678afecb367f032d93F642f64180aa3", 
                 spenderAddress : "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199", 
                 amount: BigInt(20000)
             })
-            console.log("Receipt : ", receipt)
+            if(receipt) console.log("Receipt : ", receipt)
             
             const allowance = await as.readAllowance({
                 contractAddress : "0x5FbDB2315678afecb367f032d93F642f64180aa3", 
                 ownerAddress : "0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097", 
                 spenderAddress : "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
             })
-            console.log("Allowance : ", allowance)
+            if(allowance) console.log("Allowance : ", allowance)
 
-            const erc20ts = new ERC20TokenService()
-            const tokenInfos = await erc20ts.getTokenNSymbol("0x5FbDB2315678afecb367f032d93F642f64180aa3")
-            if(tokenInfos != undefined) console.log(JSON.stringify(tokenInfos))
+            const tokenInfos = await erc20TokenService.getTokenNSymbol("0x5FbDB2315678afecb367f032d93F642f64180aa3")
+            if(tokenInfos) console.log(JSON.stringify(tokenInfos))
         }
         catch(error){
             setSupply("error")
@@ -57,6 +68,11 @@ export default function AddAllowance() {
             error : ''
         },
         spenderAddr : {
+            value : '',
+            touched : false,
+            error : ''
+        },
+        spenderName : {
             value : '',
             touched : false,
             error : ''
@@ -88,12 +104,14 @@ export default function AddAllowance() {
     }
 
     const textinputClasses = "px-[10px] mt-[6px] fill-w h-[44px] rounded-[4px] outline-1 outline outline-dashcomponent-border focus:outline-2"
+    const labelClasses = "mt-[25px] font-medium text-[#474B55]"
 
     const inputsPropsMap : Record<string, string> = {
         'amountInput' : 'allowedAmount',
         'contractInput' : 'erc20Addr',
         'ownerInput' : 'ownerAddr',
-        'spenderInput' : 'allowedAmount',
+        'spenderInput' : 'spenderAddr',
+        'spenderNameInput' : 'spenderName',
     }
 
     function handleSetInput(e: FormEvent<HTMLInputElement>): void {
@@ -116,27 +134,45 @@ export default function AddAllowance() {
         setAllowanceForm(form => ({...form, [inputsPropsMap[input.id]] : {...[inputsPropsMap[input.id]], value : input.id == "amountInput" ? parseFloat(input.value) : input.value, touched : true}}))
     }
 
+    useEffect(() => {
+        if(unlimitedAmount) return setAllowanceForm(form => ({...form, allowedAmount : {...form.allowedAmount, value : "unlimited"}}))
+        return setAllowanceForm(form => ({...form, allowedAmount : {...form.allowedAmount, value : 0}}))
+    }, [unlimitedAmount])
+
     // modal with token name, symbol
     // modal contract doesn't not exist
 
     return(
         <DashboardLayout>
             <TokenPanel/>
-            <div id="allowanceFormContainer" className='flex grow shrink flex-col bg-component-white rounded-3xl overflow-hidden p-[40px] border border-solid border-dashcomponent-border'>
-                <h1 className='mx-auto max-w-[550px] w-full text-[36px] leading-[34px] font-bold font-oswald' style={{color:'#474B55'}}>SET A NEW ALLOWANCE</h1>
-                <p className="italic mx-auto max-w-[550px] w-full mt-6 leading-snug text-[14px]">By setting this allowance, you will authorize a specific address (spender) to withdraw a fixed number of tokens from the selected ERC20 token contract. Exercise extreme caution and only grant allowances to entities you fully trust. Unlimited allowances should be avoided.</p>
-                <form className="mx-auto flex flex-col max-w-[550px] w-full">
-                    <label className="mt-[25px] font-medium" style={{color:'#474B55'}}>ERC-20 Contract Address</label>
+            <div id="allowanceFormContainer" className='flex grow shrink flex-col bg-component-white rounded-3xl overflow-hidden p-[40px] pt-[35px] border border-solid border-dashcomponent-border'>
+                <h1 className='mx-auto max-w-[580px] w-full text-[36px] leading-[34px] font-bold font-oswald' style={{color:'#474B55'}}>SET A NEW ALLOWANCE</h1>
+                <p className="border-l border-[#a2bbE0] border-solid pl-3 italic mx-auto max-w-[580px] w-full mt-6 leading-snug text-[14px]">By setting this allowance, you will authorize a specific address (spender) to withdraw a fixed number of tokens from the selected ERC20 token contract. Exercise extreme caution and only grant allowances to entities you fully trust. Unlimited allowances should be avoided.</p>
+                <form className="mx-auto flex flex-col max-w-[580px] w-full">
+                    
+                    <label className={labelClasses}>Token Contract Address</label>
                     <input id="contractInput" placeholder="0x20c...a20cb" type="text" value={allowanceForm.erc20Addr.value} className={textinputClasses} onInput={(e) => handleSetInput(e)}/>
-                    <label className="mt-[25px] font-medium" style={{color:'#474B55'}}>Owner Address</label>
+                    
+                    <label className={labelClasses}>Owner Address</label>
                     <input id="ownerInput" placeholder="0x20c...a20cb" type="text" value={allowanceForm.ownerAddr.value} className={textinputClasses} onInput={(e) => handleSetInput(e)}/>
-                    <label className="mt-[25px] font-medium" style={{color:'#474B55'}}>Spender Address</label>
+                    
+                    <label className={labelClasses}>Spender Address</label>
                     <input id="spenderInput" placeholder="0x20c...a20cb" type="text" value={allowanceForm.spenderAddr.value} className={textinputClasses} onInput={(e) => handleSetInput(e)}/>
-                    <label className="mt-[25px] font-medium" style={{color:'#474B55'}}>Amount</label>
-                    <input id="amountInput" type="number" min={0} className={textinputClasses} value={allowanceForm.allowedAmount.value} onInput={(e) => handleSetInput(e)}/>
+                    
+                    <label className={labelClasses}>Spender Name (optional)</label>
+                    <input id="spenderNameInput" placeholder="0x20c...a20cb" type="text" value={allowanceForm.spenderName.value} className={textinputClasses} onInput={(e) => handleSetInput(e)}/>
+                    
+                    <label className={labelClasses}>Amount</label>
+                    <div className="flex flex-row mt-[6px] gap-x-[15px]">
+                        <input readOnly={unlimitedAmount} id="amountInput" type={unlimitedAmount ? "text" : "number"} style={{marginTop:0}} min={0} className={textinputClasses + ' w-full' + (unlimitedAmount ? ' bg-[#DCE3F2]' : '')} value={allowanceForm.allowedAmount.value} onInput={(e) => handleSetInput(e)}/>
+                        <div onClick={() => setUnlimitedAmount(prevState => (!prevState))} className="cursor-pointer flex flex-row flex-shrink-0 items-center bg-[#DCE3F2] p-1 w-[80px] h-[44px] rounded-full shadow-[inset_0_1px_3px_#B8C9E0,0_2px_0_#ffffff]">
+                            <div className={`w-[36px] h-[36px] bg-[#474B55] rounded-full transition-all ease-in duration-150 ${unlimitedAmount ? 'ml-[36px]' : 'ml-0'}`}></div>
+                        </div>
+                    </div>
+
                     <button onClick={handleSendAllowanceForm} className="mt-[35px] font-semibold h-[44px] w-full bg-active-black rounded-[4px] text-offwhite shadow-[0_4px_8px_#5b93ec40,0_8px_16px_#5b93ec40]">Send Allowance</button>
                 </form>
-                <p className="mx-auto my-[20px]">{supply}</p>
+                { /* <p className="mx-auto my-[20px]">{supply}</p> */ }
             </div>
             <TokenPanel/>
         </DashboardLayout>
@@ -155,6 +191,11 @@ interface IFormAllowance {
         error : string
     },
     spenderAddr : {
+        value : string
+        touched : boolean
+        error : string
+    },
+    spenderName : {
         value : string
         touched : boolean
         error : string
