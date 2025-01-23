e1e50f9aafa31ae27f16b69b08c9a60ac9496adc
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-23 01:23:36 +0100

    *add* left token panel component + rows added for the add allowance page

---
+++ resources/js/Components/TokenPanel/TokenRow.tsx
@@ -0,0 +1,35 @@
+import { THexAddress } from "@/types/THexAddress"
+
+export default function TokenRow({tokenName, tokenSymbol, amount, contractAddress, imgUrl, showSeparator = true} : IProps){
+
+    function handleAddressClick(e : React.MouseEvent<HTMLSpanElement>){
+        e.preventDefault()
+
+    }
+
+    return(
+        <div className="w-full flex flex-col">
+            <div className="flex flex-row h-[68px] w-full">
+                <img src={imgUrl}/>
+                <div className="flex flex-col ml-[14px] justify-center">
+                    <span className="font-semibold translate-y-[0px] text-[#474b55]">{tokenName}</span>
+                    <span onClick={handleAddressClick} className="cursor-pointer translate-y-[1px] text-[#92949C]">{contractAddress}</span>
+                </div>
+                <div className="flex flex-col text-right ml-auto justify-center">
+                    <span className="font-semibold text-[16px] leading-[22px] font-oswald translate-y-[-2px] text-[#ADB5CEdd]">{tokenSymbol}</span>
+                    <span className="translate-y-[2px] text-[#474b55] font-medium">{amount}</span>
+                </div>
+            </div>
+            {showSeparator && <div className="h-[1px] w-full border-b-[1px] border-dashed border-[#93A9DF99]"></div>}
+        </div>
+    )
+}
+
+interface IProps{
+    tokenName : string
+    tokenSymbol : string
+    amount : number
+    contractAddress : string
+    imgUrl : string
+    showSeparator? : boolean
+}
