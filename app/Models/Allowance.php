994a70c4a455a9cead699e1e9552b7302006d46e
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-24 23:57:11 +0100

    *edit* back / Allowance model now integrating all the required properties

--- app/Models/Allowance.php
+++ app/Models/Allowance.php
@@ -3,9 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Allowance extends Model
 {
-    //
+    protected $fillable = [
+        'token_contract_id',
+        'owner_address_id',
+        'spender_address_id',
+        'amount',
+        'transaction_hash',
+    ];
+
+    protected $casts = [
+        'amount' => 'integer',
+        'token_contract_id' => 'integer',
+        'owner_address_id' => 'integer',
+        'spender_address_id' => 'integer',
+    ];
+
+    public function tokenContract()
+    {
+        return $this->belongsTo(TokenContract::class);
+    }
+
+    public function ownerAddress()
+    {
+        return $this->belongsTo(Address::class, 'owner_address_id');
+    }
+
+    public function spenderAddress()
+    {
+        return $this->belongsTo(Address::class, 'spender_address_id');
+    }
 }
-?>
--- resources/js/Components/Dashboard/Table/Table.tsx
+++ resources/js/Components/Dashboard/Table/Table.tsx
@@ -1,24 +1,22 @@
 // import trashIcon from '@/assets/icons/trashicon.svg'
 // import editIcon from '@/assets/icons/editicon.svg'
 import { useServices } from '@/hooks/useServices'
+import { IAllowance } from '@/types/IAllowance'
 import { THexAddress } from '@/types/THexAddress'
+import AddressUtils from '@/utils/AddressUtils'
 import { router } from '@inertiajs/react'
 
-export default function Table(){
+export default function Table({allowances} : {allowances : IAllowance[]}){
 
     const allowancesMockArray = Array(10).fill(0)
 
     const { erc20TokenService } = useServices()
 
-    function handleRevokeButtonClick(contractAddress : THexAddress, spenderAddress : THexAddress){
+    function handleRevokeButtonClick(allowanceId : number, contractAddress : THexAddress, spenderAddress : THexAddress){
+        // !!! show modale
         erc20TokenService.revokeAllowance({contractAddress, spenderAddress})
+        // !!! if succeed, get rid set allowance in db to 0
     }
-
-    /* get rid of */
-    const dummyContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
-    const dummyOwnerAddress = '0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097'
-    const dummySpenderAddress =  '0x14dC79964da2C08b23698B3D3cc7Ca32193d9955'
-    const dummyContractOwnerAddress = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
 
     // get allowances from DB
     // check if these allowances exists into the chain
@@ -32,22 +30,22 @@ export default function Table(){
                 </tr>
             </thead>
             <tbody>
-            {allowancesMockArray.map((allowance, index) => (
+            {allowances.map((allowance, index) => (
                 <tr key={"tableLine" + index}>
-                    <td><img className='w-[32px] mx-auto' src={`/coins/coin${index % 9}.svg`}/></td>
-                    <td>CrystalDrive</td>
-                    <td>0x587...a20cb</td>
-                    <td>CRD</td>
-                    <td>0x587...a20cb</td>
-                    <td>0x587...a20cb</td>
-                    <td>12700.00</td>
-                    <td>12/10/2024</td>
+                    <td><img className='w-[32px] mx-auto' src={`/coins/${allowance.tokenContractSymbol}.svg`}/></td>
+                    <td>{allowance.tokenContractName}</td>
+                    <td title={allowance.tokenContractAddress}>{AddressUtils.maskAddress(allowance.tokenContractAddress)}</td>
+                    <td>{allowance.tokenContractSymbol}</td>
+                    <td title={allowance.ownerAddress}>{AddressUtils.maskAddress(allowance.ownerAddress)}</td>
+                    <td title={allowance.spenderAddress}>{AddressUtils.maskAddress(allowance.spenderAddress)}</td>
+                    <td>{allowance.amount}</td>
+                    <td>12/10/2024{/* !!! updatedAt but format*/}</td>
                     <td className="flex flex-row gap-x-[10px] justify-center items-center h-[50px] px-[10px]">
-                        <button onClick={() => router.visit('editallowance/'+index)} className="flex flex-row justify-center items-center w-1/2 h-[38px] gap-x-[8px] font-semibold bg-tablebutton-bg rounded-full border-[2px] text-offblack border-offblack shadow-[0_2px_4px_#A8B0BD40,0_4px_5px_#5D81B960] hover:bg-slate-300 hover:shadow-[0_1px_0_#FFFFFF]">
+                        <button onClick={() => router.visit('editallowance/'+allowance.id)} className="flex flex-row justify-center items-center w-1/2 h-[38px] gap-x-[8px] font-semibold bg-tablebutton-bg rounded-full border-[2px] text-offblack border-offblack shadow-[0_2px_4px_#A8B0BD40,0_4px_5px_#5D81B960] hover:bg-slate-300 hover:shadow-[0_1px_0_#FFFFFF]">
                             {/*<img src={editIcon}/>*/}
-                            Update
+                            Edit
                         </button>
-                        <button onClick={() => handleRevokeButtonClick(dummyContractAddress, dummySpenderAddress)} className="flex flex-row justify-center items-center w-1/2 h-[38px] gap-x-[6px] font-semibold rounded-full bg-gradient-to-r from-[#6a6e73] to-[#8f969f] border-2 border-[#43484c] text-offwhite shadow-[0_2px_4px_#A8B0BD40,0_4px_5px_#5D81B960] textShadow hover:shadow-[0_1px_0_#FFFFFF]">
+                        <button onClick={() => handleRevokeButtonClick(allowance.id, allowance.tokenContractAddress, allowance.spenderAddress)} className="flex flex-row justify-center items-center w-1/2 h-[38px] gap-x-[6px] font-semibold rounded-full bg-gradient-to-r from-[#6a6e73] to-[#8f969f] border-2 border-[#43484c] text-offwhite shadow-[0_2px_4px_#A8B0BD40,0_4px_5px_#5D81B960] textShadow hover:shadow-[0_1px_0_#FFFFFF]">
                             {/*<img src={trashIcon}/>*/}
                             Revoke
                         </button>
@@ -72,3 +70,10 @@ export default function Table(){
 <Link href={route('editallowance', { id: 0 })}>Edit Allowance</Link>
 
 */
+
+    /* get rid of 
+    const dummyContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+    const dummyOwnerAddress = '0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097'
+    const dummySpenderAddress =  '0x14dC79964da2C08b23698B3D3cc7Ca32193d9955'
+    const dummyContractOwnerAddress = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+    */
