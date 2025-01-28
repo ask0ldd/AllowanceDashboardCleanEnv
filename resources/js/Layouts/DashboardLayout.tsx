06b72d3d549efc0293e46064e262617b18649a87
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-28 03:16:57 +0100

    *add* front / usemodalmanager hook added + dashboard layout integrating some modal management logic

--- resources/js/Layouts/DashboardLayout.tsx
+++ resources/js/Layouts/DashboardLayout.tsx
@@ -1,22 +1,54 @@
 import Header from "@/Components/Header/Header";
+import ConfirmRevocation from "@/Components/Modale/ConfirmRevocation";
+import ErrorAlert from "@/Components/Modale/ErrorAlert";
+import Modal from "@/Components/Modale/Modal";
+import SendingTransaction from "@/Components/Modale/SendingTransaction";
 import Snackbar from "@/Components/Snackbar/Snackbar";
+import useModalManager from "@/hooks/useModalManager";
+import Dashboard from "@/Pages/Dashboard";
 import { Head } from "@inertiajs/react";
+import React from "react";
 import { PropsWithChildren, ReactNode } from "react";
 
 export default function DashboardLayout({
     mainStyle,
     children,
-    success
-}: PropsWithChildren<{ mainStyle? : string, success? : string/*header?: ReactNode*/ }>) {
+    snackbarMessage,
+    modalVisibility,
+    setModalStatus,
+    errorMessageRef,
+    modalContentId
+}: PropsWithChildren<IProps>) {
 
     return(
         <div className='bg-dash-grey w-full h-full min-h-full flex flex-col font-jost'>
-            <Snackbar message={success ?? undefined}/>
+            <Snackbar message={snackbarMessage ? snackbarMessage : undefined}/>
             <Head title="Dashboard" />
             <Header/>
             <main className="flex flex-row justify-between gap-x-[30px]">
                 {children}
             </main>
+            {modalVisibility && 
+                <Modal modalVisibility={modalVisibility} setModalStatus={setModalStatus} width="560px">
+                    {{
+                        'error' : <ErrorAlert errorMessage={errorMessageRef.current}/>,
+                        'confirmRevocation' : <ConfirmRevocation/>,
+                        'sending' : <SendingTransaction/>,
+                    } [modalContentId]}
+                </Modal>
+            }
         </div>
     )
+}
+
+interface IProps{
+    setModalStatus : ({ visibility, contentId } : {
+        visibility: boolean
+        contentId?: string
+    }) => void
+    mainStyle? : string
+    snackbarMessage? : string
+    modalVisibility : boolean
+    errorMessageRef : React.RefObject<string>
+    modalContentId : string
 }
