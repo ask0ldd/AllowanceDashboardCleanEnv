838e389002fea83e075cf6448519fc6b8b585d72
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-02-02 01:46:18 +0100

    *add* front / add modal body components and enhance InfoPanel MetaMask support
    - Create reusable components for various modal bodies
    - Improve InfoPanel integration with MetaMask
    - Refactor modal structure for better maintainability

--- resources/js/Components/Header/Header.tsx
+++ resources/js/Components/Header/Header.tsx
@@ -1,11 +1,27 @@
+import { ReactNode } from "react";
 import InfoPanel from "./InfoPanel";
 import Nav from "./Nav";
 
-export default function Header(){
+export default function Header({modal} : IProps){
     return (
         <header className="flex flex-row justify-between py-5">
             <Nav />
-            <InfoPanel/>
+            <InfoPanel modal={modal}/>
         </header>
     )
+}
+
+interface IProps{
+    modal : {
+            visibility: boolean
+            setVisibility: React.Dispatch<React.SetStateAction<boolean>>
+            close: () => void
+            contentId : string
+            setContentId : React.Dispatch<React.SetStateAction<string>>
+            setStatus : ({ visibility, contentId }: { visibility: boolean, contentId?: string}) => void
+            showError : (errorMessage: string) => void
+            showInjectionModal : (injectedChild: ReactNode) => void
+            errorMessageRef : React.RefObject<string>
+            injectedComponentRef : React.RefObject<React.ReactNode>
+        }
 }
--- resources/js/Components/Header/InfoPanel.tsx
+++ resources/js/Components/Header/InfoPanel.tsx
@@ -5,10 +5,11 @@ import { THexAddress } from '@/types/THexAddress'
 import { isHexAddress } from '@/types/typeguards'
 import ClipboardUtils from '@/utils/ClipboardUtils'
 import { useSDK } from '@metamask/sdk-react'
-import { useEffect, useState } from 'react'
+import { ReactNode, useEffect, useRef, useState } from 'react'
 import { useEtherClientsContext } from '@/hooks/useEtherClientsContext'
+import logoutIcon from '@/assets/icons/logout.svg'
 
-export default function InfoPanel(){
+export default function InfoPanel({modal} : IProps){
     const { sdk, connected, provider } = useSDK()
     
     const { metamaskService, localStorageService } = useServices()
@@ -19,7 +20,8 @@ export default function InfoPanel(){
         const storageAddress = localStorageService.retrieveWalletAddress()
         return isHexAddress(storageAddress) ? storageAddress : null
     }) // !!! keeping account active when switching pages // through backend instead?
-     
+    
+    // update the wallet client and the local storage when the metamask active account changes
     useEffect(() => {
         async function setWClient(){
             const wClient = await metamaskService.getWalletClient()
@@ -35,24 +37,31 @@ export default function InfoPanel(){
         }
     }, [walletAddress])
 
+    const handleAccountsChanged = useRef<((accounts: string[]) => void) | null>(null) // memorizing the listener for later disposal
+
     useEffect(() => {
-        if (provider && connected) {
-            provider.on('accountsChanged', (accounts : any) => {
-                if(accounts.length && typeof accounts[0] == 'string' && isHexAddress(accounts[0])) {
-                    setWalletAddress(accounts[0])
-                    localStorageService.storeWalletAddress(accounts[0])
-                    return
-                }
-                setWalletAddress(null)
-                localStorageService.deleteWalletAddress()
-                flushWClient()
-            })
-            // return () => {
-            //     provider.removeListener('accountsChanged', () => {});
-            //     window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
-            // }; !!! avoid listener stackings
+        handleAccountsChanged.current = (accounts: string[]) => {
+            if(accounts.length && typeof accounts[0] == 'string' && isHexAddress(accounts[0])) {
+                setWalletAddress(accounts[0])
+                localStorageService.storeWalletAddress(accounts[0])
+                return
+            }
+            setWalletAddress(null)
+            localStorageService.deleteWalletAddress()
+            flushWClient()
         }
-    }, [provider])
+
+        if (window.ethereum) {
+            window.ethereum.on('accountsChanged', handleAccountsChanged.current)
+            return () => {
+                if (window.ethereum && handleAccountsChanged.current) {
+                    window.ethereum.removeListener('accountsChanged', handleAccountsChanged.current)
+                }
+            }
+        }
+
+        // if(!window.ethereum) handleDisconnect()
+    }, [window.ethereum])
 
     async function handleCopyToClipboard(text : string) : Promise<void> {
         await ClipboardUtils.copy(text)
@@ -61,19 +70,23 @@ export default function InfoPanel(){
 
     async function handleConnectToMetaMaskClick() {
         try {
+            // modal.showError('Check if Metamask is not asking for your credentials.')
             const accounts = await sdk?.connect()
             if(accounts && accounts.length > 0 && isHexAddress(accounts[0])) {
                 setWalletAddress(accounts[0])
                 localStorageService.storeWalletAddress(accounts[0])
             }
         } catch (err) {
+            modal.showError('Check if Metamask is not asking for your credentials.') // !!!
             console.error("Failed to connect", err)
         }
     }
 
     function handleDisconnect(){
-        sdk?.disconnect()
+        if(sdk) sdk?.terminate()
         localStorageService.fullFlush()
+        localStorageService.deleteWalletAddress()
+        flushWClient()
     }
 
     if(!connected || !walletAddress) return (
@@ -89,14 +102,34 @@ export default function InfoPanel(){
         <div className="flex flex-row gap-x-[10px] justify-center items-center h-20 bg-component-white rounded-3xl overflow-hidden p-3 px-2 border border-solid border-dashcomponent-border">
             <div className='flex justify-center items-center bg-gradient-to-r from-[#303030] to-[#4C5054] border-[1px] shadow-[0_2px_4px_#5B93EC40,0_4px_8px_#5B93EC40] border-solid border-[hsl(225,3%,20%)] w-[64px] h-[64px] rounded-[16px]'><img className='w-[42px]' src={metamaskLogo}/></div>
             <div className='flex flex-col text-[14px] text-[#303030] gap-y-[5px]' onClick={() => handleCopyToClipboard(walletAddress)}>
-                <span className='font-semibold text-[16px] text-[#BCC2C8] flex items-center gap-x-[10px]'><div className='w-[10px] h-[10px] bg-green-400 rounded-full'></div>YOUR METAMASK WALLET IS ACTIVE.</span>
+                <div className='font-semibold text-[15px] font-oswald text-[#BCC2C8] flex items-center gap-x-[10px] my-[2px]'>
+                    <div className='w-[11px] h-[11px] bg-green-400 rounded-full translate-y-[1px]'></div>
+                    <span className='tracking-wide'>YOUR METAMASK WALLET IS ACTIVE.</span>
+                </div>
                 {/*<span className='text-[13px]'>Here is your current wallet address :</span>*/}
                 <hr className='mb-[2px]'/>
-                <span className='cursor-copy'>{walletAddress}</span>
+                <span className='cursor-copy hover:bg-[#e8ebed]'>{walletAddress}</span>
             </div>
-            <button className='flex justify-center items-center flex-shrink-0 flex-grow-0 bg-[#E8EBED] w-[64px] h-[64px] rounded-[16px]' onClick={handleDisconnect}>disco</button>
+            <button className='flex justify-center items-center flex-shrink-0 flex-grow-0 bg-[#E8EBED] w-[64px] h-[64px] rounded-[16px]' onClick={handleDisconnect}>
+                <img className='w-[32px] h-[32px]' src={logoutIcon}/>
+            </button>
         </div>
     )
+}
+
+interface IProps{
+    modal : {
+            visibility: boolean
+            setVisibility: React.Dispatch<React.SetStateAction<boolean>>
+            close: () => void
+            contentId : string
+            setContentId : React.Dispatch<React.SetStateAction<string>>
+            setStatus : ({ visibility, contentId }: { visibility: boolean, contentId?: string}) => void
+            showError : (errorMessage: string) => void
+            showInjectionModal : (injectedChild: ReactNode) => void
+            errorMessageRef : React.RefObject<string>
+            injectedComponentRef : React.RefObject<React.ReactNode>
+    }
 }
 
     /*const requestMade = useRef(false)
