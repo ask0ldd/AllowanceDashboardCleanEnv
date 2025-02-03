9b3285e57f1d4243dad6c8cc24c87e824c0aeda0
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-02-03 03:10:18 +0100

    *add* front & back : Implement WebSocket communication infrastructure
    - Install Laravel Echo for real-time client-side events
    - Set up Reverb server for WebSocket broadcasting
    - Integrate Pusher as fallback broadcast driver
    - Configure both frontend and backend for WebSocket supports

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AddressServiceProvider::class,
     App\Providers\AllowanceServiceProvider::class,
     App\Providers\TokenServiceProvider::class,
+    App\Providers\TransactionHashServiceProvider::class,
 ];
--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -2,11 +2,16 @@ import { PageProps as InertiaPageProps } from '@inertiajs/core';
 import { AxiosInstance } from 'axios';
 import { route as ziggyRoute } from 'ziggy-js';
 import { PageProps as AppPageProps } from './';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
 
 declare global {
     interface Window {
         axios: AxiosInstance;
         ethereum?: EIP1193Provider
+        Echo: Echo
+        Pusher: typeof Pusher
     }
 
     /* eslint-disable no-var */
