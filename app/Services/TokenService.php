33a4e49a732a6b37de326126337e92a2f0a7b2cf
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-02-08 08:55:04 +0100

    *edit* back / Add PHPDoc to Scraping, Token & TransactionHash services
    - Enhance code documentation for better readability and maintainability

--- app/Services/TokenService.php
+++ app/Services/TokenService.php
@@ -5,8 +5,19 @@ namespace App\Services;
 use App\Models\TokenContract;
 use App\Http\Resources\TokenContractResource;
 
+/**
+ * Service class for handling token-related operations.
+ */
 class TokenService
 {
+    /**
+     * Retrieve a collection of token contracts.
+     *
+     * This method fetches the first 10 token contracts with their associated addresses,
+     * and returns them as a collection of TokenContractResource objects.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function getAll(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection // \Illuminate\Contracts\Pagination\LengthAwarePaginator
     {
         $tokens = TokenContract::with('address')->take(10)->get();
